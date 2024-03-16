d9c72a9351973c87c97eea3c7213417516c96e08
carlosbotero22 <monoprograma22@gmail.com>
2024-03-15 22:37:05 -0500

    cambiar color de fondo

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <div :style="{ backgroundColor: colorDeFondo }" class="area-cambiable">
+        <p>Área de cambio de color</p>
+      </div>
+  
+      <button @click="cambiarColor('white')">Blanco</button>
+      <button @click="cambiarColor('black')">Negro</button>
+      <button @click="cambiarColor('blue')">Azul</button>
+      <button @click="cambiarColor('green')">Green</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CambioColorComponent',
+    data() {
+      return {
+        colorDeFondo: '' // Color inicial
+      };
+    },
+    methods: {
+      cambiarColor(color) {
+        this.colorDeFondo = color;
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .area-cambiable {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  </style>
+  
+
